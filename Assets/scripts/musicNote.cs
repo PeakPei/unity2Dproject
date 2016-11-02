7fbcb3fcae14944021cd6500cad3d732bec51133
gl14916 <garyli14916@live.com>
2016-11-02 02:31:11 -0400

    Collision Detectable
    Every time you hit a note, the Console says "Score 1000!"

--- Assets/scripts/musicNote.cs
+++ Assets/scripts/musicNote.cs
@@ -1,16 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnitySampleAssets;
+using UnitySampleAssets._2D;
 
 public class musicNote : MonoBehaviour {
 
-    HudScript hud;
+    
+    GameObject gameObj;
 
-    void OnTriggerEnter2d(Collider2D other)
+    void Start()
     {
-        if(other.tag == "Player")
+        //hud = GameObject.Find("MainCamera").GetComponent<HudScript>();
+        //hud.IncreaseScore(1000);
+    }
+    
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        PlatformerCharacter2D character = other.gameObject.GetComponent<PlatformerCharacter2D>();
+        if (character.gameObject.name == "CharacterRobotBoy")
         {
-            hud = GameObject.Find("MainCamera").GetComponent<HudScript>();
-            hud.IncreaseScore(1000);
+            //hud = GameObject.Find("MainCamera").GetComponent<HudScript>();
+            //hud.IncreaseScore(1000);
+            Debug.Log("Score 1000!");
             Destroy (this.gameObject);
         }
     }
