5cfa6ebcd94451cce3d3221c26ad0c0ff5a929bb
aerobins12994 <aerobins@umich.edu>
2016-11-02 15:22:37 -0400

    UI

---
+++ Assets/scripts/startMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class startMenu : MonoBehaviour {
+
+    public Button playB;
+    public Button how2play;
+    public Button ready2play;
+
+	// Use this for initialization
+	void Start () {
+        playB.onClick.AddListener(() =>
+       {
+           Application.LoadLevel("myScene");
+       });
+
+        how2play.onClick.AddListener(() =>
+       {
+           Application.LoadLevel("how2play");
+
+       });
+
+        ready2play.onClick.AddListener(() =>
+        {
+            Application.LoadLevel("myScene");
+        });
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
